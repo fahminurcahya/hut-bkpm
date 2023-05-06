642eb8f9bf52e0f5fd1954ef6f1aa5369cd6306d
fahminurcahya <fahminurcahya@gmail.com>
2023-05-07 00:49:28 +0700

    model pendamping

---
+++ models/Pendamping.js
@@ -0,0 +1,30 @@
+const { DataTypes, Sequelize } = require("sequelize");
+const sequelize = require("../configs/db");
+
+const Pendamping = sequelize.define(
+  "pendamping",
+  {
+    id_peserta: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    id_pendamping: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ukuran: {
+      type: DataTypes.CHAR,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: false,
+  }
+);
+
+module.exports = Pendamping;
