8f59916ffae27818f8570b2f9f394df72ca4da7d
fahminurcahya <fahminurcahya@gmail.com>
2023-05-07 02:55:48 +0700

    api regis

--- controllers/api/PesertaController.js
+++ controllers/api/PesertaController.js
@@ -1,4 +1,10 @@
-const { send, sendQRPNG, sendQRBase64, sendQRAttach } = require("../../mail");
+const {
+  send,
+  sendQRPNG,
+  sendQRBase64,
+  sendQRAttach,
+  generatePDF,
+} = require("../../mail");
 const Peserta = require("../../models/Peserta");
 const { generateQRPNG } = require("../../utils/generateQRPNG");
 // const { QueryTypes } = require("sequelize");
@@ -9,12 +15,10 @@ const {
   responseException,
 } = require("../../utils/handlerResponse");
 
-const {
-  sendQRAttachWithGeneratePdf,
-  sendNotifAdmin,
-} = require("../../mail");
+const { sendQRAttachWithGeneratePdf, sendNotifAdmin } = require("../../mail");
 var QRCode = require("qrcode");
 const sequelize = require("../../configs/db");
+const Pendamping = require("../../models/Pendamping");
 
 const register = async (req, res) => {
   const {
@@ -30,7 +34,19 @@ const register = async (req, res) => {
     departement,
     alamat,
     flag_internal,
+    golongan_darah,
+    flag_racepack,
+    flag_checkin,
+    nama_darurat,
+    hubungan,
+    alamat_darurat,
+    no_hp_darurat,
+    nik,
+    pendamping,
   } = req.body;
+  console.log(pendamping);
+  const t = await sequelize.transaction();
+
   try {
     let dataPeserta = {
       no_peserta: 101,
@@ -47,9 +63,72 @@ const register = async (req, res) => {
       alamat,
       flag_internal,
       qr_code: "",
+      golongan_darah,
+      flag_racepack,
+      nik,
+      flag_checkin,
+      nama_darurat,
+      hubungan,
+      no_hp_darurat,
+      alamat_darurat,
     };
 
-    const peserta = await Peserta.create(dataPeserta);
+    const peserta = await Peserta.create(dataPeserta, { transaction: t });
+
+    // if (event === "fw" && flag_internal) {
+    //   // let dataPendamping = {};
+    //   let dataPendamping = {};
+    //   console.log(pendamping.length);
+    //   for (let i = 0; i < pendamping.length; i++) {
+    //     dataPendamping.id_peserta = peserta.id;
+    //     dataPendamping.id_pendamping = i + 1;
+    //     dataPendamping.nama = pendamping[i].nama_pendamping;
+    //     dataPendamping.ukuran = pendamping[i].ukuran;
+    //     console.log(dataPendamping);
+    //     const pendampings = await Pendamping.create(dataPendamping, {
+    //       transaction: t,
+    //     });
+    //   }
+    //   console.log(dataPendamping);
+    // }
+
+    if (event === "fw" && flag_internal) {
+      const {
+        nama_pendamping_1,
+        nama_pendamping_2,
+        nama_pendamping_3,
+        nama_pendamping_4,
+        nama_pendamping_5,
+        ukuran_1,
+        ukuran_2,
+        ukuran_3,
+        ukuran_4,
+        ukuran_5,
+      } = req.body;
+
+      let dataPendamping = {};
+      let namap = "nama_pendamping_";
+      for (let i = 1; i <= 5; i++) {
+        if (
+          this["nama_pendamping_" + i] != undefined ||
+          this["nama_pendamping_" + i] != null ||
+          this["nama_pendamping_" + i] != ""
+        ) {
+          dataPendamping.id_peserta = peserta.id;
+          dataPendamping.id_pendamping = i;
+          dataPendamping.nama = `${namap}` + `${i}`;
+          dataPendamping.ukuran = this["ukuran_" + i];
+          console.log(dataPendamping);
+          const pendamping = await Pendamping.create(dataPendamping);
+        }
+      }
+    }
+    // throw new Error("daad");
+
+    await generateQRPNG(peserta.no_peserta, peserta.qr_code, peserta.event);
+    await generatePDF(email, peserta.no_peserta, peserta.nama, peserta.event);
+
+    await t.commit();
 
     let result = {
       peserta,
@@ -60,28 +139,17 @@ const register = async (req, res) => {
     // peserta.qr = qr;
     // await sendQRBase64(email, peserta);
 
-    await generateQRPNG(peserta.no_peserta, peserta.qr_code);
-    // await generatePDF(peserta.no_peserta, event);
+    // await generateQRPNG(peserta.no_peserta, peserta.qr_code);
+    // // await generatePDF(peserta.no_peserta, event);
 
-    // await sendQRPNG(email, peserta);
-    // await sendQRAttach(email, peserta);
-    await sendQRAttachWithGeneratePdf(email, peserta.no_peserta, event);
-    await sendNotifAdmin(email, peserta.no_peserta, peserta.nama, event);
+    // // await sendQRPNG(email, peserta);
+    // // await sendQRAttach(email, peserta);
+    // await sendQRAttachWithGeneratePdf(email, peserta.no_peserta, event);
+    // await sendNotifAdmin(email, peserta.no_peserta, peserta.nama, event);
 
     res.status(200).json(responseSukses(result));
   } catch (err) {
-    if (err.name !== "validation") {
-      await Peserta.findOne({
-        where: { email },
-      });
-
-      await Peserta.destroy({
-        where: {
-          email,
-        },
-        force: true,
-      });
-    }
+    await t.rollback();
 
     console.log(err);
     res.status(500).json(responseException(err.message));
