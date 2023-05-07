ac3f49cee1ca7aa54ddaa1c96743b3642e7cd4cc
fahminurcahya <fahminurcahya@gmail.com>
2023-05-07 15:43:44 +0700

    controller register dan handle radio button pendamping

--- controllers/PesertaController.js
+++ controllers/PesertaController.js
@@ -104,6 +104,7 @@ const register = async (req, res) => {
     alamat_darurat,
     no_hp_darurat,
     nik,
+    pendamping,
   } = req.body;
   const t = await sequelize.transaction();
 
@@ -130,61 +131,64 @@ const register = async (req, res) => {
       hubungan,
       no_hp_darurat,
       alamat_darurat,
+      pendamping,
     };
     const peserta = await Peserta.create(dataPeserta, { transaction: t });
 
     if (event === "fw" && flag_internal) {
-      const {
-        nama_pendamping_1,
-        nama_pendamping_2,
-        nama_pendamping_3,
-        nama_pendamping_4,
-        nama_pendamping_5,
-        ukuran_1,
-        ukuran_2,
-        ukuran_3,
-        ukuran_4,
-        ukuran_5,
-      } = req.body;
+      if (pendamping == "Y") {
+        const {
+          nama_pendamping_1,
+          nama_pendamping_2,
+          nama_pendamping_3,
+          nama_pendamping_4,
+          nama_pendamping_5,
+          ukuran_1,
+          ukuran_2,
+          ukuran_3,
+          ukuran_4,
+          ukuran_5,
+        } = req.body;
 
-      let dataPendamping = {};
-      let namap = [];
-      for (let x = 1; x <= 5; x++) {
-        switch (x) {
-          case 1:
-            namap.push({ nama: nama_pendamping_1, ukuran: ukuran_1 });
-            break;
-          case 2:
-            namap.push({ nama: nama_pendamping_2, ukuran: ukuran_2 });
-            break;
-          case 3:
-            namap.push({ nama: nama_pendamping_3, ukuran: ukuran_3 });
-            break;
-          case 4:
-            namap.push({ nama: nama_pendamping_4, ukuran: ukuran_4 });
-            break;
-          case 5:
-            namap.push({ nama: nama_pendamping_5, ukuran: ukuran_5 });
-            break;
+        let dataPendamping = {};
+        let namap = [];
+        for (let x = 1; x <= 5; x++) {
+          switch (x) {
+            case 1:
+              namap.push({ nama: nama_pendamping_1, ukuran: ukuran_1 });
+              break;
+            case 2:
+              namap.push({ nama: nama_pendamping_2, ukuran: ukuran_2 });
+              break;
+            case 3:
+              namap.push({ nama: nama_pendamping_3, ukuran: ukuran_3 });
+              break;
+            case 4:
+              namap.push({ nama: nama_pendamping_4, ukuran: ukuran_4 });
+              break;
+            case 5:
+              namap.push({ nama: nama_pendamping_5, ukuran: ukuran_5 });
+              break;
+          }
         }
-      }
 
-      console.log(namap);
+        console.log(namap);
 
-      for (let i = 0; i < namap.length; i++) {
-        if (
-          namap[i].nama != "" &&
-          namap[i].nama != undefined &&
-          namap[i].nama != null
-        ) {
-          dataPendamping.id_peserta = peserta.id;
-          dataPendamping.id_pendamping = i + 1;
-          dataPendamping.nama = namap[i].nama;
-          dataPendamping.ukuran = namap[i].ukuran;
-          console.log(dataPendamping);
-          const pendamping = await Pendamping.create(dataPendamping, {
-            transaction: t,
-          });
+        for (let i = 0; i < namap.length; i++) {
+          if (
+            namap[i].nama != "" &&
+            namap[i].nama != undefined &&
+            namap[i].nama != null
+          ) {
+            dataPendamping.id_peserta = peserta.id;
+            dataPendamping.id_pendamping = i + 1;
+            dataPendamping.nama = namap[i].nama;
+            dataPendamping.ukuran = namap[i].ukuran;
+            console.log(dataPendamping);
+            const pendamping = await Pendamping.create(dataPendamping, {
+              transaction: t,
+            });
+          }
         }
       }
     }
